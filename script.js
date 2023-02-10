975a59c128ea9d9174d4ef884f7ac78161b74b1d
Mohamed Kadri <mo.jalil@Mohameds-MacBook-Pro-2.local>
2023-02-10 20:10:13 +0200

    Initial Script

---
+++ script.js
@@ -0,0 +1,16 @@
+// Fix Oxygen Builder Editor Layout for RTL sites
+// Get the current window location
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const oxy_builder = urlParams.get("ct_builder");
+
+// Check if the current page is an editor page
+if (oxy_builder == "true") {
+  // if it's the editor, make the page direction LTR
+  // Making the page direction LTR will ensure that the builder itself will show up correctly
+  // While the page's content will remain RTL
+  // It works!
+  document
+    .querySelector('html[ng-app="CTFrontendBuidlerUI"]')
+    .setAttribute("dir", "ltr");
+}
